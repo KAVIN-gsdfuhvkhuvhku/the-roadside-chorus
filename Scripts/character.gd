cc55097b29eb65ea57e1e7bc54ad4e2327302b5f
KAVIN-gsdfuhvkhuvhku <23191@my.westlakegirls.school.nz>
2026-08-18 23:07:19 +1200

    Update character.gd

--- Scripts/character.gd
+++ Scripts/character.gd
@@ -3,15 +3,12 @@ extends Node2D
 @onready var sprite = $AnimatedSprite2D
 
 const CHARACTER_FRAMES = {
-	"You" : preload("res://Resources/Protagonist_sprites.tres")
-	"Grandma" : preload("res://Resources/Grandma_sprites.tres")
+	"You": preload("res://Resources/Protagonist_sprites.tres"),
+	"Grandma": preload("res://Resources/Grandma_sprites.tres")
 	}
 
-
-static func get_enum_from_string(string_value: String) -> int:
-	var upper_string = string_value.to_upper()
-	if Name.has(upper_string):
-		return Name[upper_string]
-	else:
-		push_error("Invalid Character name: " + string_value)
-		return -1
+func change_character(name: String, is_talking : bool = true):
+	animated_sprite.sprite_frames = CHARACTER_FRAMES[name]
+	if is_talking:
+		animated_sprite.play("talking")
+	e;se
